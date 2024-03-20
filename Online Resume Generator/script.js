fba77b01d8426d3601adf722da7f49576fe1d271
praneethallichetty <134947500+praneethallichetty@users.noreply.github.com>
2024-03-20 21:02:57 +0530

    Add files via upload

---
+++ Online Resume Generator/script.js
@@ -0,0 +1,117 @@
+function addNewWEField()
+{
+    //console.log("Adding New Field");
+    let newNode = document.createElement("textarea") 
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "enter here");
+
+    let weOb = document.getElementById("we");
+    let weAddButtonOb = document.getElementById("weAddButton");
+
+    weOb.insertBefore(newNode, weAddButtonOb);
+}
+
+
+function addNewAQField()
+{
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("eqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter here");
+    
+    let aqOb = document.getElementById("aq")
+    let aqAddButtonOb = document.getElementById("aqAddButton");
+
+    aqOb.insertBefore(newNode, aqAddButtonOb);
+
+}
+
+
+//generating CV
+function  generateCV()
+{
+ // console.log("generating CV");
+
+ let nameField = document.getElementById("nameField").value;
+
+ let nameT1 = document.getElementById("nameT1");
+
+ nameT1.innerHTML = nameField;
+
+ document.getElementById('nameT2').innerHTML=nameField;
+
+ document.getElementById("contactT").innerHTML = document.getElementById(
+    "contactField"
+    ).value;
+
+ document.getElementById("addressT").innerHTML = document.getElementById(
+    "addressField"
+    ).value;
+
+ document.getElementById("LinkedT").innerHTML = document.getElementById(
+    "LinkedField"
+    ).value;
+
+ document.getElementById("fbT").innerHTML = document.getElementById(
+        "fbField").value;
+
+  document.getElementById("instaT").innerHTML = document.getElementById(
+         "instaField").value;
+ 
+
+ document.getElementById("objectiveT").innerHTML = document.getElementById(
+    "objectiveField"
+     ).value;
+
+   //we
+
+   let wes = document.getElementsByClassName("weField");
+
+   let str = "";
+
+   for (let e of wes)  {
+      str = str + '<li> ${e.value} </li>' ;             
+
+
+   }
+
+   document.getElementById("weT").innerHTML = str;
+
+   //aq
+
+   let aqs = document.getElementsByClassName("eqField");
+
+   let str1 = "";
+
+   for (let e of aqs)  {
+      str1 += '<li> ${e.value} </li>';
+   }
+
+   document.getElementById("aqT").innerHTML = str1;
+
+   document.getElementById("cv-form").style.display = "none";
+   document.getElementById("cv-template").style.display = "block";
+}
+
+//print cv
+function printCV() {
+   window.print();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
